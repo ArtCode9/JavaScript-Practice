5732ba69c9c314634eb81ef4e7672bf945a3caeb
ArtCode9 <alirezatavanmand91@gmail.com>
2024-12-24 11:44:45 -0800

    call and apply methods

--- js/jsM2.js
+++ js/jsM2.js
@@ -117,6 +117,146 @@ document.body.addEventListener('click', high5); // addEventListener is higher-or
  */
 
 /////////////////////////////////////////////////////////////
-/////           ////
+/////          7 Functions Returning Functions           ////
 /////////////////////////////////////////////////////////////
+
+/* 
+
+const greet = function (greeting) {
+   return function (name) {
+         console.log(`${greeting} ${name}`);
+   };
+};
+
+const greeterHey = greet('hey');
+greeterHey('Art');
+greeterHey('Steven');
+
+greet('hi')('dada');
+
+//  challenge
+//  write arrow function for above functions
+const greetArr = greeting => names => console.log(`${greeting} ${names}`);
+
+greetArr('Hiiii')('Arrow Functions');
+
+ */
+
+/////////////////////////////////////////////////////////////
+/////////       8 The call and apply Methods           //////
+/////////////////////////////////////////////////////////////
+
+/* 
+
+const lufthansa = {
+   airline: 'Lufthansa',
+   iataCode: 'LH',
+   bookings: [],
+   // book: function()
+   book(flightNum, name) {
+         console.log(`${name} booked a seat on ${this.airline} flight
+            ${this.iataCode} ${flightNum}`);
+   this.bookings.push({flight: `${this.iataCode}${flightNum}`, name });         
+   },
+};
+
+lufthansa.book(239, 'Jonas Schmedtman');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+
+const euroWings = {
+   airline: 'Eurowings',
+   iataCode: 'EW',
+   bookings: []
+};
+
+const book = lufthansa.book;
+// lufthansa.book.call(euroWings, 23, 'Sarah Williams');
+
+
+// Does NOT work
+// book(23, 'Sarah Williams');
+
+//  Call Methods
+book.call(euroWings, 23, 'Sarah Williams');
+console.log(euroWings);
+
+
+book.call(lufthansa, 23, 'Sarah Williams');
+console.log(lufthansa);
+
+const swiss = {
+   airline: 'Swiss Air line',
+   iataCode: 'LX',
+   bookings: []  
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+//  Apply Methods
+const flightDate = [583, 'George Cooper'];
+book.apply(swiss, flightDate);
+console.log(swiss);
+
+book.call(swiss, ...flightDate);
+
+
+// The apply() method accepts arguments in an array:
+const person = {
+  fullName: function(city, country) {
+    return this.firstName + " " + this.lastName + "," + city + "," + country;
+  }
+}
+
+const person1 = {
+  firstName:"John",
+  lastName: "Doe"
+}
+
+person.fullName.apply(person1, ["Oslo", "Norway"]);
+
+
+
+//   Compared with the call() method:
+const person = {
+  fullName: function(city, country) {
+    return this.firstName + " " + this.lastName + "," + city + "," + country;
+  }
+}
+
+const person1 = {
+  firstName:"John",
+  lastName: "Doe"
+}
+
+person.fullName.call(person1, "Oslo", "Norway");
+
+// another Example for call() method
+
+const person = {
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  }
+}
+const person1 = {
+  firstName:"John",
+  lastName: "Doe"
+}
+const person2 = {
+  firstName:"Mary",
+  lastName: "Doe"
+}
+
+// This will return "John Doe":
+person.fullName.call(person1);
+
+ */
+
+/////////////////////////////////////////////////////////////
+/////////            9 The bind Method                 //////
+/////////////////////////////////////////////////////////////
+
+
 
