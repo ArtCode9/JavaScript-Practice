51da5bfd93d50d96dd1ad62134f8a56416f35c75
ArtCode9 <alirezatavanmand91@gmail.com>
2024-12-31 11:28:43 -0800

    End of 10

--- js/jsM2.js
+++ js/jsM2.js
@@ -519,5 +519,98 @@ console.dir(booker);
 
  */
 
+/* 
+// Example 1 ::
+let f;
+
+const g = function () {
+   const a =23;
+   f = function () {
+      console.log(a * 2);
+   }
+};
+
+const h = function () {
+   const b = 777;
+   f = function() {
+      console.log(b * 2);
+   }
+};
+
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+
+// Example 2 ::
+const boardPassengers = function(n, wait) {
+      const perGroup = n / 3;
+
+      setTimeout(function(){
+         console.log(`We are now boarding all ${n}
+            passengers`);
+         console.log(`There are 3 groups, each with ${perGroup} passengers`);   
+      }, wait * 1000);
+
+
+      console.log(`Will start boarding in ${wait} seconds`);
+};
+
+// const perGroup = 1000;
+
+boardPassengers(180, 6);
+
+
+ */
+
+/////////////////////////////////////////////////////////////
+///////////             14 CHALLENGE #2              /////////
+/////////////////////////////////////////////////////////////
+
+
+
+
+// Your tasks: 
+// 1. Take the IIFE below and at the end of the function, attach an event listener that 
+// changes the color of the selected h1 element ('header') to blue, each time 
+// the body element is clicked. Do not select the h1 element again! 
+// 2. And now explain to yourself (or someone around you) why this worked! Take all 
+// the time you need. Think about when exactly the callback function is executed, 
+// and what that means for the variables involved in this example.
+
+
+(function () { 
+   const header = document.querySelector('h1'); 
+   header.style.color = 'red'; 
+
+   document.querySelector('body').addEventListener('click',
+      function() {
+         header.style.color= 'blue';
+      }
+   );
+})(); 
+
+
+
+/////////////////////////////////////////////////////////////
+///////////                   END                   /////////
+/////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
 
 
