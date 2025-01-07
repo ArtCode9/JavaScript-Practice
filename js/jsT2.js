df4c992df84e476a2ef50d6144dace58c589d068
ArtCode9 <alirezatavanmand91@gmail.com>
2025-01-07 11:04:39 -0800

    Button section build

--- js/jsT2.js
+++ js/jsT2.js
@@ -475,6 +475,25 @@ window.addEventListener('resize', () =>
 initParticles(1600);
 animate();
 
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////     box 7       ///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+// sx1
 
+const button = document.querySelector('.neon-button');
+button.addEventListener('click', function (e) {
+      const ripple = document.createElement('span');
+            ripple.classList.add('ripple');
+      const rect = button.getBoundingClientRect();
+      const size = Math.max(rect.width, rect.height);
 
+         ripple.style.width = ripple.style.height = `${size}px`;
+         ripple.style.left = `${e.clientX - rect.left - size / 2}px`;
+         ripple.style.top = `${e.clientY - rect.top - size / 2}px`;
 
+         button.appendChild(ripple);
+
+   ripple.addEventListener('animationend', () => ripple.remove());
+});
+
+// sx2
