aea0ab8c64e71cfe03599019a76db2254bf35c7a
ArtCode9 <alirezatavanmand91@gmail.com>
2025-01-11 18:47:10 -0800

    Operations With Dates end

--- js/NumbersDatesandTimer.js
+++ js/NumbersDatesandTimer.js
@@ -23,9 +23,9 @@ const account1 = {
      '2020-01-28T09:15:04.904Z',
      '2020-04-01T10:17:24.185Z',
      '2020-05-08T14:11:59.604Z',
-     '2020-05-27T17:01:17.194Z',
-     '2020-07-11T23:36:17.929Z',
-     '2020-07-12T10:51:36.790Z',
+     '2020-01-11T17:01:17.194Z',
+     '2025-01-08T23:36:17.929Z',
+     '2025-01-10T10:51:36.790Z',
    ],
    currency: 'EUR',
    locale: 'pt-PT', // de-DE
@@ -82,7 +82,28 @@ const account1 = {
  
  /////////////////////////////////////////////////
  // Functions
+
+const formatMovementDate = function(date) {
  
+  const calcDaysPassed = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if(daysPassed === 0) return 'Today'; 
+  if(daysPassed === 1) return 'Yesterday'; 
+  if(daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+  
+    return `${day}/${month}/${year}`;
+  }; 
+  };
+ 
+
  const displayMovements = function (acc, sort = false) {
    containerMovements.innerHTML = '';
  
@@ -92,11 +113,8 @@ const account1 = {
      const type = mov > 0 ? 'deposit' : 'withdrawal';
  
      const date = new Date(acc.movementsDates[i]);
-     const day = `${date.getDate()}`.padStart(2, 0);
-     const month = `${date.getMonth() + 1}`.padStart(2, 0);
-     const year = date.getFullYear();
+     const displayDate  = formatMovementDate(date);
 
-     const displayDate = `${day}/${month}/${year}`;
 
      const html = `
        <div class="movements__row">
@@ -302,6 +320,17 @@ labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
 ///////////            10 Operations With Dates           ////////////
 //////////////////////////////////////////////////////////////////////
 
+
+/* 
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const calcDaysPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
+console.log(days1);
+
+ */
 
 
 //////////////////////////////////////////////////////////////////////
