52f820b8d7f286a4a4e2d4c2f3cc077bcca48bf2
ArtCode9 <alirezatavanmand91@gmail.com>
2025-01-25 11:08:56 -0800

    select create and delete with js

--- js/AdvancedDOMandEvents.js
+++ js/AdvancedDOMandEvents.js
@@ -20,10 +20,11 @@ const closeModal = function() {
    overlay.classList.add('hidden');
 };
 
-// for(let i = 0; i < btnsOpenModal.length; i++)
-//    btnsOpenModal[i].addEventListener('click', openModal);
+for(let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', openModal);
+
 // do same as commented above code
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));// do same code above
+//btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));// do same code above
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -38,5 +39,59 @@ document.addEventListener('keydown', function(e) {
 /////////////////////   004 How the DOM Really Works    //////////////////
 //////////////////////////////////////////////////////////////////////////
 
+//  check your note and read a lot MDN Dom  
 
 
+//////////////////////////////////////////////////////////////////////////
+//////////   005 Selecting, Creating, and Deleting Elements    ///////////
+//////////////////////////////////////////////////////////////////////////
+
+// selecting  elements :::
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+console.log(`============`);
+
+const header = document.querySelector('.header');
+console.log(header);
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+console.log(`============`);
+
+console.log(document.getElementById('section--1'));
+console.log(`============`);
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+console.log(`============`);
+
+const className = document.getElementsByClassName('btn');
+console.log(className);
+
+
+// creating and inserting elements
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookied for improved functionality and analytics';
+message.innerHTML = `We use cookied for improved functionality and analytics.
+                     <button class="btn btn--close-cookie">Got it!</button>`;
+// we select header above
+//header.prepend(message); // add before first child
+header.append(message);
+// header.append(message.cloneNode(true)); // add to last child and copy node
+
+header.before(message);
+// header.after(message);
+
+
+// Delete elements
+document.querySelector('.btn--close-cookie')
+         .addEventListener('click', function (){
+            //message.remove();
+            message.parentElement.removeChild(message);
+         });
+
+//////////////////////////////////////////////////////////////////////////
+//////////       ///////////
+//////////////////////////////////////////////////////////////////////////
