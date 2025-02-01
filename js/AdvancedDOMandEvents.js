d6d0a4089aa7b298a3433a5f5d3e6c6b49b1f1ad
ArtCode9 <alirezatavanmand91@gmail.com>
2025-02-01 02:10:42 -0800

    img effect

--- js/AdvancedDOMandEvents.js
+++ js/AdvancedDOMandEvents.js
@@ -537,7 +537,7 @@ const allSection = document.querySelectorAll('.section');
 
 const revealSection = function(entries, observer){
       const [entry] = entries;
-      console.log(entry);
+      // console.log(entry);
 
       if(!entry.isIntersecting) return;
       
@@ -560,3 +560,29 @@ allSection.forEach(function(section){
 ///////////////           018 Lazy Loading Images             ////////////
 //////////////////////////////////////////////////////////////////////////
 
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+// console.log(imgTargets);
+const loadImg = function(entries, observer){
+   const [entry] = entries;
+   console.log(entry);
+
+   if(!entry.isIntersecting) return;
+
+   // replace src with data-src
+   entry.target.src = entry.target.dataset.src;
+   
+   entry.target.addEventListener('load', function(){
+      entry.target.classList.remove('lazy-img');
+   });
+
+   observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+      root: null,
+      threshold: 0,
+      rootMargin: '200px'
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
