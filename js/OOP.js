605fa64beb7c101270ee8a9cb1cb892e5e504377
ArtCode9 <alirezatavanmand91@gmail.com>
2025-02-12 12:57:27 -0800

    OOP Done

--- js/OOP.js
+++ js/OOP.js
@@ -1331,35 +1331,44 @@ The Account class in JavaScript provides a
   transaction history or interest calculations.
 
  */
-/* 
+
 class Account {
    constructor(owner, currency, pin){
       this.owner = owner;
       this.currency = currency;
-      this.pin = pin;
-      this.movements = [];
+      // protected property
+      this._pin = pin;
+      this._movements = [];
+
       this.locale = navigator.language;
 
       console.log(`Thanks for opening an account, ${owner}`);
    }
 
    // public interface
+   getMovements() {
+      return this._movements;
+   }
+
    deposit(val){
-      this.movements.push(val);
+      this._movements.push(val);
+      return this;
    }
 
    withdraw(val){
       this.deposit(-val);
+      return this;
    }
 
-   approveLoan(val){
+   _approveLoan(val){
       return true;
    }
 
    requestLoan(val){
-      if(this.approveLoan(val)){
+      if(this._approveLoan(val)){
          this.deposit(val);
          console.log(`Loan approved.`);
+         return this;
       }
    }
 };
@@ -1367,24 +1376,147 @@ class Account {
 const acc1 = new Account('Jonas', 'EUR', 1111);
 console.log(acc1);
 
-//  acc1.movements.push(250);
-//  acc1.movements.push(-149);
+//   this will protected so cant effected from outside
+//  acc1._movements.push(250);
+//  acc1._movements.push(-149);
+
 // we do instead like this
 acc1.deposit(290);
 acc1.withdraw(145);
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
+acc1._approveLoan(1000);
+console.log('😂' + acc1.getMovements());
 
 console.log(acc1);
-console.log(acc1.pin);
-console.log(acc1.movements);
+console.log(acc1._pin);
+console.log(acc1._movements);
 console.log(acc1.deposit);
+ 
+/////////////////////////////////////////////////////////////////////
+////   020 Encapsulation Protected Properties and Methods   /////////
+/////////////////////////////////////////////////////////////////////
+// all change for this section on last section 19 all code (protected)
 
+/////////////////////////////////////////////////////////////////////
+////////   021 Encapsulation Private Class Fields and Methods   /////
+/////////////////////////////////////////////////////////////////////
+
+/* 
+// 1. Public fields
+// 2. Private fields
+// 3. Public methods
+// 4. Private methods
+// (there is also the static version)
+
+class Account2 {
+   // 1. Public fields (instance)
+   locale = navigator.language;
+   
+   // 2. Private fields (instance) 
+   #movements = [];
+   #pin; 
+
+   // 3. Public methods
+
+   static helper(){
+      console.log(`Helper`);
+   }
+
+   // 4. Private methods
+   #approveLoan(val){
+      return true;
+   }
+};
  */
 
 /////////////////////////////////////////////////////////////////////
-////   020 Encapsulation Protected Properties and Methods   ////////
+//////////////       022 Chaining Methods                ////////////
 /////////////////////////////////////////////////////////////////////
+
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
+
+/////////////////////////////////////////////////////////////////////
+///////////           024 CHALLENGE #4                ///////////////
+/////////////////////////////////////////////////////////////////////
+
+// Coding Challenge #4
+
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} is going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+console.log(rivian);
+// console.log(rivian.#charge);
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate();
+
+console.log(rivian.speedUS);
+
 
 
 
