7b18174b46d17dd915e6dcdea921838bc0afb93a
ArtCode9 <artcoode9@gmail.com>
2025-07-01 11:57:00 -0700

    Geolo API

---
+++ js/MaptyAppOop.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+// using the Geolocation API
+if(navigator.geolocation){ 
+navigator.geolocation.getCurrentPosition(function (position){
+    // console.log(position);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(latitude, longitude);
+    console.log(`https://www.google.pt/maps/@${latitude}, ${longitude}`);  // this is our currnet location on google map
+    
+
+} , function () {
+    alert(`Could Not get your position`);
+});
+}
