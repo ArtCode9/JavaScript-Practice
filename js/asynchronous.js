6e07cc0c7de1b0f97bc4fc93c3952dd980da5c61
artcode9 <artcoode9@gmail.com>
2025-11-15 08:40:53 -0800

    Done

--- js/asynchronous.js
+++ js/asynchronous.js
@@ -1,17 +1,44 @@
 'use strict'
 
 // const btn = document.querySelector('.btnCountry');
-const text = document.getElementById('text');
+// const text = document.getElementById('text');
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
-request.send();
+// const request = new XMLHttpRequest();
+// request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
+// request.send();
 
 
-request.addEventListener('load', function(){
- const [data] = JSON.parse(this.responseText);
- console.log(data);
+// request.addEventListener('load', function(){
+//  const [data] = JSON.parse(this.responseText);
+//  console.log(data);
 
    
-});
+// });
 // =======================================
+// for git remember : 1) git add .   2) git commit -m "your message"  3) git pull  4) git push
+
+
+const renderCountry = function (data){
+   const html = `
+      <article class="country">
+       <img class="">
+      </article>
+   `;
+}
+
+const getCountryAndNeighbour = function (country) {
+   const request = new XMLHttpRequest();
+   
+   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+   request.send();
+
+   request.addEventListener('load', function () {
+       const  [data] = JSON.parse(this.responseText);
+       console.log(data);
+
+       renderCountry(data);
+   });
+
+};
+
+getCountryAndNeighbour();
