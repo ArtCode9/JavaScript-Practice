c313710129766cfb7115f8ac7a132a1e8560950a
artcode9 <artcoode9@gmail.com>
2026-02-19 05:32:17 -0800

    add code

--- 2026/Exe/exe2 - Fight/script.js
+++ 2026/Exe/exe2 - Fight/script.js
@@ -15,8 +15,53 @@ document.body.addEventListener('keydown', function(e) {
       imgs1.src = "./src/ryu-hadouken.gif";
       setTimeout(() => {
          imgs1.src = "./src/ryustance.gif";
-      }, 1990);
-      
+      }, 1790);
+      if(Number(score1.innerText) <= 0) {
+         winner.innerText = `🙎Ken is the Winner🏅`;
+         score2.innerText = 100;
+         score1.innerText = 100;
+      } else if(Number(score2.innerText) <= 0) {
+         winner.innerText = `🙅Ryu is the winner 🥇`;
+         score2.innerText = 100;
+         score1.innerText = 100;
+      }   
+   
+   }
+   else if(e.key == 'p') 
+   {
+      score1.innerText -= Number(Math.floor(Math.random() * 10));
+      imgs2.src = "./src/ezken2.gif";
+      setTimeout(() => {
+         imgs2.src = "./src/KenStance.gif";
+      }, 500);
+      if(Number(score1.innerText) <= 0) {
+         winner.innerText = `🙎Ken is the Winner🏅`;
+         score2.innerText = 100;
+         score1.innerText = 100;
+      } else if (Number(score2.innerText) <= 0) {
+         winner.innerText = `🙅Ryu is the winner 🥇`;
+         score2.innerText = 100;
+         score1.innerText = 100;
+      }
+
+   }
+    else if(e.key == 'a')
+   {
+      if(score1.innerText != 100) {
+         score1.innerText = Number(score1.innerText) + Number(Math.floor(Math.random() * 10));
+         if(score1.innerText > 100) {
+            score1.innerText = Number(100);
+         }
+      }
    } 
+    else if(e.key == 'l') 
+   {  
+      if(score2.innerText != 100) {
+         score2.innerText = Number(score2.innerText) + Number(Math.floor(Math.random() * 10));
+         if(score2.innerText > 100) {
+            score2.innerText = Number(100);
+         }
+      }
+   }  
 
 });
